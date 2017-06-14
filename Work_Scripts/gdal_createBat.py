7d39e6a3797a769d3d35cab772398898ad4c0ef4
Wes Porter <wesporter92@gmail.com>
2017-06-14 13:44:06 -0500

    Works for entire MS landsat collection now

--- Work_Scripts/gdal_createBat.py
+++ Work_Scripts/gdal_createBat.py
@@ -9,7 +9,7 @@
 
 ws = "E:/Wes/Work/USDA/tmp/"
 
-in_file = open(ws +"ndvi_test2.txt",'r')
+in_file = open(ws +"ndvi.txt",'r')
 files = []
 for line in in_file:
     strFromFile = line.strip() # Remove line breaks
@@ -43,22 +43,51 @@ for name in files:
 
     spry.append(name[0:13])
 
+# Grab unique spry combinations
 sprySet = set(spry)
-setList = list(sprySet)
+sprySet = sorted(sprySet)
+spryList = list(sprySet)
 
-spryList = []
-
+rowGroups = []
 # Create separate lits for each unique Landsat row in collection
-for i in setList:
+for i in spryList:
     fileList = []
     for name in files:
         if (i in name):
             fileList.append(name)
-    spryList.append(fileList)
+    rowGroups.append(fileList)
 
-for i in spryList[0]:
-    for j in spryList[1]:
-        doyI = i[13:16]
-        doyJ = j[13:16]
-        if (doyI == doyJ):
-            out_string = "gdalwarp -t_srs EPSG:26916 " + str(i) + " " + str(j) + " " + str(i[0:6]) + str(i[9:16])+ "mosaic.tif"
+# Create text file to send formatted string to.
+outFile = open(ws + "MS_gdal_mosaic.txt",'w')
+
+# Write header info
+l1 = ":: WES PORTER\n"
+l2 = ":: 6/8/2017\n"
+l3 = ":: USDA PROJECT\n"
+l4 = ":: Summary: Working with GDAL to reproject and mosaic NDVI data.\n"
+l5 = "echo off\n"
+l6 = "title GDAL NDVI MOSAIC\n"
+header = l1+l2+l3+l4+l5+l6
+outFile.write(header)
+
+for i in spryList:
+    for j in spryList:
+        spI = i[0:6]
+        rowI = i[6:9]
+        yI = i[9:13]
+        spJ = j[0:6]
+        rowJ = j[6:9]
+        yJ = j[9:13]
+        if (spI == spJ and yI == yJ and int(rowI) == (int(rowJ)-1)):
+            # Format GDAL arguments for *possible mosaic combinations.
+            for i in rowGroups[0]:
+                for j in rowGroups[1]:
+                    doyI = i[13:16]
+                    doyJ = j[13:16]
+                    if (doyI == doyJ):
+                        out_string = "gdalwarp -t_srs EPSG:26916 " + str(i) + " " + str(j) + " " + str(i[0:6]) + str(i[9:16])+ "_msc.tif" + "\n"
+                        outFile.write(out_string)
+
+outFile.close()
+
+print("FINI\n")
