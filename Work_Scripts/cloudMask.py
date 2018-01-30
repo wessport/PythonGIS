9006e8afbdc521611a98bc55a1aa35edd5f477ef
Wes Porter <wesporter92@gmail.com>
2018-01-30 11:21:20 -0600

    Created cloudMask.py

---
+++ Work_Scripts/cloudMask.py
@@ -0,0 +1,93 @@
+# WES PORTER
+# 29-JAN-2018
+# USDA PROJECT - cloudMask.py
+
+# SCRIPT SUMMARY:
+# Working with arcpy to mask clouds from SR_NDVI
+
+import arcpy, arcinfo
+import datetime
+from arcpy.sa import *
+from os import listdir
+
+
+# System time
+now = datetime.datetime.now()
+
+# Disable: 'Add results of geoprocessing operations to the display'
+arcpy.env.addOutputsToMap = 0
+
+# Set output Coordianate System to NAD83 UTM Z16
+arcpy.env.outputCoordinateSystem = "PROJCS['NAD_1983_UTM_Zone_16N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-87.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]"
+
+# Set snapRaster
+arcpy.env.snapRaster = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_mosaiced/LT05_L1TP_023036_19950615_20160927_01_T1_sr_ndvi_prj.tif"
+
+# Cell Size
+arcpy.env.cellSize = 30.0
+
+# Setting our workspace/environment
+ws = "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/tmp"
+arcpy.env.workspace = ws
+
+#Location of sr_ndvi
+ndvi_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_mosaiced/"
+
+#Location of pixel_qa band
+pixel_qa_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/PIXEL_QA_mosaiced/"
+
+# Create list of file names
+ndvi_list = []
+items = listdir(ndvi_loc)
+for i in items:
+    if i.endswith(".tif"):
+        ndvi_list.append(i)
+ndvi_list.sort()
+
+pixel_qa_list = []
+items = listdir(pixel_qa_loc)
+for i in items:
+    if i.endswith(".tif"):
+        pixel_qa_list.append(i)
+pixel_qa_list.sort()
+
+# Create a dictionary for ndvi, pixel_qa pairs
+img_dictionary = dict(zip(ndvi_list,pixel_qa_list))
+
+# Mask our sr_ndvi
+out_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_msc_cloudFree/"
+
+arcpy.CheckOutExtension("spatial")
+
+for ndvi, pixel_qa in img_dictionary.items():
+    # Define which pixel values in the pixel_qa band to mask output
+    # Arcpy argument must be a string
+
+    # argument = 'SetNull(("{0}"== 224) | ("{0}"== 176) | ("{0}"== 160) | ("{0}"== 144) | ("{0}"== 136) | ("{0}"== 132) | ("{0}"== 112) | ("{0}"== 96) | ("{0}"== 80) | ("{0}"== 72) | ("{0}"== 68), | ("{0}"== 1), "{1}")'.format(pixel_qa, ndvi)
+    #
+    # output_raster = arcpy.sa.RasterCalculator(argument); output_raster.save(out_loc + "{}_masked".format(ndvi))
+
+    output_raster = Con(((pixel_qa_loc + pixel_qa) == 224) | ((pixel_qa_loc + pixel_qa) == 176) | ((pixel_qa_loc + pixel_qa) == 160) | ((pixel_qa_loc + pixel_qa) == 144) | ((pixel_qa_loc + pixel_qa) == 136) | ((pixel_qa_loc + pixel_qa) == 132) | ((pixel_qa_loc + pixel_qa) == 112) | ((pixel_qa_loc + pixel_qa) == 96) | ((pixel_qa_loc + pixel_qa) == 80) | ((pixel_qa_loc + pixel_qa) == 72) | ((pixel_qa_loc + pixel_qa) == 68) | ((pixel_qa_loc + pixel_qa) == 1), -9999, (ndvi_loc + ndvi))
+
+    output_raster.save(out_loc + "{}_masked.tif".format(ndvi[:-4]))
+
+print("\n TASK COMPLETED:" + now.strftime("%Y-%m-%d %H:%M") + "\n")
+
+
+# pixel_qa_loc = pixel_qa_loc + "/" + "LT05_L1TP_023036_19950615_20160927_01_T1_pixel_qa_prj.tif"
+# ndvi_loc = ndvi_loc + "/" + "LT05_L1TP_023036_19950615_20160927_01_T1_sr_ndvi_prj.tif"
+#
+# pixel_qa = "LT05_L1TP_023036_19950615_20160927_01_T1_pixel_qa_prj.tif"
+# ndvi = "LT05_L1TP_023036_19950615_20160927_01_T1_sr_ndvi_prj.tif"
+#
+# arcpy.CheckOutExtension("spatial")
+#
+# outRas = Con((pixel_qa_loc == 224) | (pixel_qa_loc == 176) , -9999, ndvi_loc)
+#
+# outRas.save(out_loc + "{}_masked.tif".format(ndvi))
+
+# | (pixel_qa == 160) | (pixel_qa == 144) | (pixel_qa == 136) | (pixel_qa == 132) | (pixel_qa == 112) | (pixel_qa == 96) | (pixel_qa == 80) | (pixel_qa == 72) | (pixel_qa == 68) | (pixel_qa == 1)
+
+arcpy.CheckInExtension("spatial")
+
+arcpy.ResetEnvironments()
---
+++ Work_Scripts/gdal_mosaicBat.py
@@ -0,0 +1,58 @@
+# WES PORTER
+# 24-JAN-2018
+# USDA PROJECT - gdal_mosaicBat.py
+
+# SCRIPT SUMMARY:
+# Working with python to generate a GDAL bat file to mosaic list of ESPA images.
+
+# C:\Python27\ArcGISx6410.3\python.exe gdal_createBat.py
+import datetime
+now = datetime.datetime.now()
+
+ws = "E:/Wes/Work/USDA/tmp/"
+
+in_file = open(ws + "pixel_qa_files.txt", 'r')
+files = []
+for line in in_file:
+    strFromFile = line.strip()  # Remove line breaks
+    files.append(strFromFile)
+
+# Create text file to send formatted string to
+outFile = open(ws + "MS_gdal_mosaic.txt", 'w')
+
+# Write header info
+l1 = ":: WES PORTER\n"
+l2 = ":: " + now.strftime("%Y-%m-%d ") + "\n"
+l3 = ":: USDA PROJECT\n"
+l4 = ":: Summary: Working with GDAL to reproject and mosaic NDVI data.\n"
+l5 = "echo off\n"
+l6 = "title GDAL NDVI MOSAIC\n"
+header = l1 + l2 + l3 + l4 + l5 + l6
+outFile.write(header)
+
+gdal = "C:/OSGeo4W64/bin/gdalwarp.exe"  # GDAL location
+proj = '"+proj=utm +zone=16 +datum=NAD83"'  # Projection
+
+# Grab matching landsat rows, write GDAL arguments
+for i in files:
+    for j in files:
+        sI = i[0:4]
+        pI = i[10:13]
+        rowI = i[13:16]
+        dateI = i[17:25]
+        sJ = j[0:4]
+        pJ = j[10:13]
+        rowJ = j[13:16]
+        dateJ = j[17:25]
+
+        if (sI == sJ and pI == pJ and dateI == dateJ and int(rowI) == (int(rowJ) - 1)):
+            out_string = gdal + " -t_srs " + proj + " " + \
+                str(i) + " " + str(j) + " " + \
+                str(i[0:13]) + str(i[16:25]) + "_pixel_qa_msc.tif" + "\n"
+            outFile.write(out_string)
+
+footer = "echo Execution complete.\nPause"
+outFile.write(footer)
+outFile.close()
+
+print("\n ~~~FINI~~~ \n")
