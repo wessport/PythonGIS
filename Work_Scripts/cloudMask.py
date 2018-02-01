e062ae96b0b194b420bd7eaba72066be29d4cc99
Wes Porter <wesporter92@gmail.com>
2018-02-01 12:11:27 -0600

    Changed order that snapRaster defined.
    Noticed row and col were incorrect for ND mask.  Corrected Central_Meridian error as well - should have been -99.0.

--- Work_Scripts/cloudMask.py
+++ Work_Scripts/cloudMask.py
@@ -5,109 +5,124 @@
 # SCRIPT SUMMARY:
 # Working with arcpy to mask clouds from SR_NDVI
 
-import sys, getopt
+import sys, os
 import arcpy, arcinfo
 import datetime
 from arcpy.sa import *
 from os import listdir
 
+def cloud_mask(state):
+    """
+    NOTE: In order for output to work NAS hould be mapped to Z:/ drive.
+    Assuming no other drive is mapped in which case NAS may not map to Z:/.
+    Masks clouds from NDVI by creating a binary masking raster.
+    Uses ArcPy Reclassify and Con tools for the masking operations.
+    SetProperties tool is used to strictly define nodata as -9999 instead of
+    ESRI default nodata value.
+    Outputs results to the NAS in respective state's NDVI directory.
+    """
 
-# System time
-now = datetime.datetime.now()
+    # Disable: 'Add results of geoprocessing operations to the display'
+    arcpy.env.addOutputsToMap = 0
 
-# Disable: 'Add results of geoprocessing operations to the display'
-arcpy.env.addOutputsToMap = 0
+    if state == 'MS':
+        # Set output Coordianate System to NAD83 UTM Z16
+        arcpy.env.outputCoordinateSystem = "PROJCS['NAD_1983_UTM_Zone_16N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-87.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]"
 
-# Set output Coordianate System to NAD83 UTM Z16
-arcpy.env.outputCoordinateSystem = "PROJCS['NAD_1983_UTM_Zone_16N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-87.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]"
+        # Setting our workspace/environment
+        ws = "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/tmp"
+        arcpy.env.workspace = ws
 
-# Cell Size
-arcpy.env.cellSize = 30.0
+        # Location of sr_ndvi
+        ndvi_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_mosaiced/"
 
-# Setting our workspace/environment
-ws = "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/tmp"
-arcpy.env.workspace = ws
+        # Check to make sure NAS is mapped to Z:/ drive
+        if os.path.isdir(ndvi_loc) == False:
+            print("NAS may not be currently mapped. Attempting to map drive...")
+            os.system('pushd \\161.45.156.61\Public')
+            if os.path.isdir(ndvi_loc) == True:
+                print("NAS has been succesfully mapped.")
 
-#Location of sr_ndvi
-ndvi_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_mosaiced/"
+        # Location of pixel_qa band
+        pixel_qa_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/PIXEL_QA_mosaiced/"
 
-#Location of pixel_qa band
-pixel_qa_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/PIXEL_QA_mosaiced/"
+        # Output file location
+        out_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_msc_cloudFree/"
 
-# Create list of file names
-ndvi_list = []
-items = listdir(ndvi_loc)
-for i in items:
-    if i.endswith(".tif"):
-        ndvi_list.append(i)
-ndvi_list.sort()
+    elif state == 'ND':
+        arcpy.env.outputCoordinateSystem = "PROJCS['NAD_1983_UTM_Zone_14N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-99.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]"
 
-pixel_qa_list = []
-items = listdir(pixel_qa_loc)
-for i in items:
-    if i.endswith(".tif"):
-        pixel_qa_list.append(i)
-pixel_qa_list.sort()
+        # Setting our workspace/environment
+        ws = "E:/Wes/Work/USDA/raw/North_Dakota/ND_NDVI/tmp"
+        arcpy.env.workspace = ws
 
-# Create a dictionary for ndvi, pixel_qa pairs
-img_dictionary = dict(zip(ndvi_list,pixel_qa_list))
+        #Location of sr_ndvi
+        ndvi_loc = "Z:/Wes/USDA/Data/North_Dakota/ND_NDVI/ESPA_NDVI/unzipped/SR_NDVI_projected/"
 
-# Mask our sr_ndvi
-out_loc = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_msc_cloudFree/"
+        #Location of pixel_qa band
+        pixel_qa_loc = "Z:/Wes/USDA/Data/North_Dakota/ND_NDVI/ESPA_NDVI/unzipped/PIXEL_QA_projected/"
 
-arcpy.CheckOutExtension("spatial")
+        # Output file location
+        out_loc = "Z:/Wes/USDA/Data/North_Dakota/ND_NDVI/ESPA_NDVI/unzipped/SR_NDVI_masked/"
 
-for ndvi, pixel_qa in img_dictionary.items():
-    # Define which pixel values in the pixel_qa band to mask output
-    # Arcpy argument must be a string
+    # Cell Size
+    arcpy.env.cellSize = 30.0
 
-    # Create temporary binary cloud raster
-    reclass_pixel_qa = arcpy.sa.Reclassify(pixel_qa_loc + pixel_qa, "Value", "66 0;68 1;72 1;96 1;130 0;136 1;160 1;224 1", "DATA")
+    # Create list of file names
+    ndvi_list = []
+    items = listdir(ndvi_loc)
+    for i in items:
+        if i.endswith(".tif"):
+            ndvi_list.append(i)
+    ndvi_list.sort()
 
-    output_raster = Con((reclass_pixel_qa == 1) | IsNull(reclass_pixel_qa) | (reclass_pixel_qa == -32768), -9999, ndvi_loc + ndvi)
+    pixel_qa_list = []
+    items = listdir(pixel_qa_loc)
+    for i in items:
+        if i.endswith(".tif"):
+            pixel_qa_list.append(i)
+    pixel_qa_list.sort()
 
-    # Set snapRaster
-    arcpy.env.snapRaster = reclass_pixel_qa
+    if len(ndvi_list) == len(pixel_qa_list):
+        # Create a dictionary for ndvi, pixel_qa pairs
+        img_dictionary = dict(zip(ndvi_list,pixel_qa_list))
 
-    arcpy.management.SetRasterProperties(output_raster, None, None, None, "1 -9999", None)
+    else:
+        return('NDVI list and pixel_qa list are different lengths!')
 
-    output_raster.save(out_loc + "{}_masked.tif".format(ndvi[:-4]))
+    # Mask our sr_ndvi
+    start_time = datetime.datetime.now()
 
-    # Can't have more than 6 conditional statements inside one Con function
-    # output_raster = Con(((pixel_qa_loc + pixel_qa) == 224) | ((pixel_qa_loc + pixel_qa) == 176) | ((pixel_qa_loc + pixel_qa) == 160) | ((pixel_qa_loc + pixel_qa) == 144) | ((pixel_qa_loc + pixel_qa) == 136) | ((pixel_qa_loc + pixel_qa) == 132) | ((pixel_qa_loc + pixel_qa) == 112) | ((pixel_qa_loc + pixel_qa) == 96) | ((pixel_qa_loc + pixel_qa) == 80) | ((pixel_qa_loc + pixel_qa) == 72) | ((pixel_qa_loc + pixel_qa) == 68) | ((pixel_qa_loc + pixel_qa) == 1), -9999, (ndvi_loc + ndvi))
+    arcpy.CheckOutExtension("spatial")
 
-    # output_raster.save(out_loc + "{}_masked.tif".format(ndvi[:-4]))
+    for ndvi, pixel_qa in img_dictionary.items():
 
-print("\n TASK COMPLETED:" + now.strftime("%Y-%m-%d %H:%M") + "\n")
+        # !! Arcpy argument must be a string !!
 
+        # Define which pixel values in the pixel_qa band to mask output
+        # Create temporary binary cloud raster in memory
+        reclass_pixel_qa = arcpy.sa.Reclassify(pixel_qa_loc + pixel_qa, "Value", "66 0;68 1;72 1;96 1;130 0;136 1;160 1;224 1", "DATA")
 
-# pixel_qa_loc = pixel_qa_loc + "/" + "LT05_L1TP_023036_19950615_20160927_01_T1_pixel_qa_prj.tif"
-# ndvi_loc = ndvi_loc + "/" + "LT05_L1TP_023036_19950615_20160927_01_T1_sr_ndvi_prj.tif"
+        # Set snapRaster
+        arcpy.env.snapRaster = reclass_pixel_qa
 
-# pixel_qa = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/PIXEL_QA_mosaiced/LT05_L1TP_023_19850923_pixel_qa_msc.tif"
-# ndvi = "Z:/Wes/USDA/Data/Mississippi/MS_NDVI/ESPA_NDVI/unzipped/SR_NDVI_mosaiced/LT05_L1TP_023_19850923_msc.tif"
-#
-# arcpy.CheckOutExtension("spatial")
-#
-# outRas = Con((pixel_qa_loc == 224) | (pixel_qa_loc == 176) , -9999, ndvi_loc)
-#
-# outRas.save(out_loc + "{}_masked.tif".format(ndvi))
+        output_raster = Con((reclass_pixel_qa == 1) | IsNull(reclass_pixel_qa) | (reclass_pixel_qa == -32768), -9999, ndvi_loc + ndvi)
 
-# | (pixel_qa == 160) | (pixel_qa == 144) | (pixel_qa == 136) | (pixel_qa == 132) | (pixel_qa == 112) | (pixel_qa == 96) | (pixel_qa == 80) | (pixel_qa == 72) | (pixel_qa == 68) | (pixel_qa == 1)
+        # Define nodata value as -9999
+        arcpy.management.SetRasterProperties(output_raster, None, None, None, "1 -9999", None)
 
-# reclass_pixel_qa = arcpy.sa.Reclassify(pixel_qa, "Value", "66 0;68 1;72 1;96 1;130 0;136 1;160 1;224 1", "DATA")
-#
-# outRas = Con((reclass_pixel_qa == 1) | IsNull(reclass_pixel_qa) | (reclass_pixel_qa == -32768), -9999, ndvi)
-#
-# # Set snapRaster
-# arcpy.env.snapRaster = reclass_pixel_qa
-#
-# arcpy.management.SetRasterProperties(outRas, None, None, None, "1 -9999", None)
-#
-# outRas.save(out_loc + "{}_maskedTEST.tif".format("LT05_L1TP_023_19850923_msc"))
+        output_raster.save(out_loc + "{}_masked.tif".format(ndvi[:-4]))
 
-arcpy.CheckInExtension("spatial")
+    # Current system time
+    now = datetime.datetime.now()
 
-arcpy.ResetEnvironments()
+    elapsed_time = (float(start.strftime("%S")) - float(now.strftime("%S"))) / 60
+
+    print("\n TASK COMPLETED:" + now.strftime("%Y-%m-%d %H:%M") + "\n" + "ELAPSED TIME: " + str(elapsed_time) + "\n")
+
+    arcpy.CheckInExtension("spatial")
+
+    arcpy.ResetEnvironments()
 
 if __name__ == '__main__':
+    cloud_mask(sys.argv[1])
