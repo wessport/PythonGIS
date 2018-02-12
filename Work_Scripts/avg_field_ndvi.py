4ea9cf63db950f9bd155b7aecd90f6bcc523a901
Wes Porter <wesporter92@gmail.com>
2018-02-12 12:20:11 -0600

    Modified avg field ndvi script to handle new ESPA images.
    Added ntpath to trim paths off of image file names. Added error handling for the dateparser when it encounters strings containing more than one date.

--- Work_Scripts/avg_field_ndvi.py
+++ Work_Scripts/avg_field_ndvi.py
@@ -1,5 +1,5 @@
 # WES PORTER
-# 7/20/2017
+# 12-FEB-2018
 # USDA PROJECT - avg_field_ndvi.py
 
 # SCRIPT SUMMARY:
@@ -7,19 +7,26 @@
 
 import arcpy
 import glob
+import ntpath
+import datetime
+import dateutil.parser as dparser
 
 # Disable: 'Add results of geoprocessing operations to the display'
 arcpy.env.addOutputsToMap = 0
 
 # Setting our workspace/environment
-ws = "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/test_field"
+ws = "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/tmp/test_field"
 arcpy.env.workspace = ws
 
-ndvi_loc = "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/MS_Cloudfree_Null"
+arcpy.env.overwriteOutput = True
 
-years = ["1985","1990","1995"]
+ndvi_loc = "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/SR_NDVI_msc_cloudFree"
+
+arcpy.CheckOutExtension("spatial")
+
+years = ["1985","1990","1995","2000"]
 for year in years:
-    image_list =  glob.glob(ndvi_loc+"/LT5023{}*_msc_null.tif".format(year))
+    image_list =  glob.glob(ndvi_loc+"/*_{}*_masked.tif".format(year))
     Id_list = []
     DOY_list = []
     Mean_list = []
@@ -31,9 +38,16 @@ for year in years:
     Header = 'FIELD_ID,DOY,AVG_NDVI,MAX' +'\n'
     out_file.write(Header)
 
+    test_field_loc = "E:/Wes/Work/USDA/raw/Mississippi/MS_Shapefiles/MS_Field_Boundaries/test_field.shp"
+
     for i in image_list:
-        doy = i[-16:-13]
-        arcpy.gp.ZonalStatisticsAsTable_sa("test_field", "Id", i, "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/test_field/{}_{}.dbf".format(year,doy), "DATA", "ALL")
+        try:
+            image_date = dparser.parse(ntpath.basename(i), fuzzy=True)
+        except ValueError:
+             sys.exit('ValueError encountered. Parse should only be called on a string containing one date')
+
+        doy = image_date.timetuple().tm_yday
+        arcpy.gp.ZonalStatisticsAsTable_sa(test_field_loc, "Id", i, "E:/Wes/Work/USDA/raw/Mississippi/MS_NDVI/tmp/test_field/{}_{}.dbf".format(year,doy), "DATA", "ALL")
 
         # Create our search cursor
         table_loc = ws + "/{}_{}.dbf".format(year,doy)
@@ -56,9 +70,12 @@ for year in years:
         del SC
 
     for i in range(0,len(Mean_list)):
-        out_string = str(Id_list[i])+','+DOY_list[i]+','+str(Mean_list[i])+','+str(Max_list[i])+'\n'
+        out_string = str(Id_list[i])+','+str(DOY_list[i])+','+str(Mean_list[i])+','+str(Max_list[i])+'\n'
         out_file.write(out_string)
 
     out_file.close()
+
+arcpy.CheckInExtension("spatial")
+arcpy.ResetEnvironments()
 
 print("\n ~~~FINI~~~ \n")
